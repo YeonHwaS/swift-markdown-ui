574fc45f2114034c0fb70a390374052b08e74bb7
Ren Shin <ren.shin@swit.io>
2023-12-10 16:24:45 +0900

    cmark-gfm update

--- Package.swift
+++ Package.swift
@@ -21,11 +21,35 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.10.0"),
   ],
   targets: [
-    .target(name: "cmark-gfm"),
+    .target(name: "cmark-gfm",
+      path: "Sources/cmark-gfm/src",
+      exclude: [
+        "scanners.re",
+        "libcmark-gfm.pc.in",
+        "config.h.in",
+        "CMakeLists.txt",
+        "cmark-gfm_version.h.in",
+        "case_fold_switch.inc",
+        "entities.inc",
+      ],
+      cSettings: []
+    ),
+    .target(name: "cmark-gfm-extensions",
+      dependencies: [
+        "cmark-gfm",
+      ],
+      path: "Sources/cmark-gfm/extensions",
+      exclude: [
+        "CMakeLists.txt",
+        "ext_scanners.re",
+      ],
+      cSettings: []
+    ),
     .target(
       name: "MarkdownUI",
       dependencies: [
         "cmark-gfm",
+        "cmark-gfm-extensions",
         .product(name: "NetworkImage", package: "NetworkImage"),
       ]
     ),
